fef8122f19029bff0f8d2883bb8024e13461b0b2
Xavier ADABUNU <xad@raadvst-consetat.be>
2024-06-03 09:40:44 +0200

    fix : set dappercontext and DocumentRepo as Singleton

--- FileSafetyService/Program.cs
+++ FileSafetyService/Program.cs
@@ -4,8 +4,8 @@ builder.Services.AddHostedService<Worker>();
 builder.Services.AddSingleton<IConnectionFactory, ConnectionFactory>();
 builder.Services.AddSingleton<IQueueService, QueueService>();
 
-builder.Services.AddScoped<DapperContext>();
-builder.Services.AddScoped<IRepository<Document>, DocumentRepository>();
+builder.Services.AddSingleton<DapperContext>();
+builder.Services.AddSingleton<IRepository<Document>, DocumentRepository>();
 
 var host = builder.Build();
 host.Run();
